f47fd71b4fbeb4b977c6a94c761a8101b141b142
IgMan <33981565+ignmandagaran@users.noreply.github.com>
2019-09-20 00:00:24 -0300

    Update enviar_consulta.php

--- vendors/phpmail/enviar_consulta.php
+++ vendors/phpmail/enviar_consulta.php
@@ -21,8 +21,8 @@
         $mail->isSMTP();                                            // Set mailer to use SMTP
         $mail->Host       = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'manzmetalurgica@gmail.com';                     // SMTP username
-        $mail->Password   = 'elcano8254';                               // SMTP password
+        $mail->Username   = 'confidencial';                     // SMTP username
+        $mail->Password   = 'confidencial';                               // SMTP password
         $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
         $mail->Port       = 587;                                    // TCP port to connect to
 
